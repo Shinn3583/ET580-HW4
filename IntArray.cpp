82dd56d8df80c626f9cd70d449e69388333d8c5c
Zhiyi Chen 3583 <calphzay0822@gmail.com>
2025-11-07 00:43:38 +0000

    Implement part of function definitions in intArray.cpp 11/6 Zhiyi Chen

--- IntArray.cpp
+++ IntArray.cpp
@@ -7,3 +7,44 @@
 
 // Initialize static member
 int IntArray::total_entries = 0;
+
+// Zhiyi Chen 11/6 Commit3 implementing  function definitions in cpp files.
+IntArray::IntArray() : size(5) // Default constructor
+{
+    arr = new int[size];
+    total_entries += size;
+}
+
+
+IntArray::IntArray(int s) : size(s)  // Parameterized constructor
+{
+    arr = new int[size];
+    total_entries += size;
+}
+
+
+IntArray::~IntArray()  // Destructor
+{
+    delete[] arr;
+}
+
+
+int IntArray::getSize() // Get size of array
+{
+    return size;
+}
+
+
+int IntArray::getValue(int index) // Get value at index
+{
+    assert(index >= 0 && index < size);
+    return arr[index];
+}
+
+
+void IntArray::replace(int index, int value) // Replace value at indexS
+{
+    if (index >= 0 && index < size) {
+        arr[index] = value;
+    }
+}
